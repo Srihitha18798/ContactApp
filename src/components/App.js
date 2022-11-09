1b02503da734586ad8879d0431935694633ada62
srihitha <srihithatsr@gmail.com>
2022-11-09 17:43:59 +0530

    Updated code(react hooks)

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Header from './Header';
 import AddContact from './AddContact';
@@ -6,23 +6,17 @@ import ContactList from './ContactList';
 
 function App() {
 
-  const contacts=[
-    {
-      id:"1",
-      name:"Srihitha",
-      email:"srihitha@gmail.com"
-    },
-    {
-      id:"2",
-      name:"Tadiparthi",
-      email:"tadiparthi@gmail.com"
-    },
-  ];
+  const[contacts, setContacts]=useState([]);
+
+  const addContactHandler=(contact)=>{
+    console.log(contact);
+    setContacts([...contacts,contact]);
+  };
   return (
     <div className="ui container">
       <Header />
-      <AddContact />
-      <ContactList contacts={contacts}/>
+      <AddContact addContactHandler={addContactHandler}/>
+      <ContactList contacts={contacts} />
     </div>
   );
 }
